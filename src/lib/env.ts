6c6421761730c4cedc2407b4aea87be83b0a3cd3
Romain Bourdain <romain.bourdain93@gmail.com>
2025-01-29 16:31:31 +0100

    setup database schemas

---
+++ src/lib/env.ts
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    POSTGRES_USER: z.string().min(1),
+    POSTGRES_PASSWORD: z.string().min(1),
+    POSTGRES_HOST: z.string().min(1),
+    POSTGRES_PORT: z.string().min(1),
+    POSTGRES_DB: z.string().min(1),
+    POSTGRES_SCHEMA: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env,
+});
