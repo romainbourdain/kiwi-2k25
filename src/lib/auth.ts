7e943b5059c4f0a6d41cd4a67a0601a9e37034a2
Romain Bourdain <romain.bourdain93@gmail.com>
2025-01-29 15:48:01 +0100

    configure authentication and db

---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { db } from "./db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+  adapter: PrismaAdapter(db),
+});
